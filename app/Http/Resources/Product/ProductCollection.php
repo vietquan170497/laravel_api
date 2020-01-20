9648f1268a839681174860af885548768bc6b30a
Viết Quân <vietquan170497@gmail.com>
2020-01-20 15:48:34 +0700

    Review Resource & Product pagination

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -18,6 +18,7 @@ class ProductCollection extends Resource
             'name' => $this->name,
             'totalPrice' => round((1-$this->discount/100)*$this->price,2),
             'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'No rating yet',
+            'discount' => $this->discount,
             'href' => [
                 'reviews' => route('products.show',$this->id)
             ]
